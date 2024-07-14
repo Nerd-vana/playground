792a01ee09629ff06947bd69d66d5fbcd43b4fe2
BarryChum <barrychum@users.noreply.github.com>
2024-07-14 18:18:05 +0100

    updated for common parameters

--- asciinema2mp4/config.ts
+++ asciinema2mp4/config.ts
@@ -1,12 +1,15 @@
   // config.ts
+  // https://docs.asciinema.org/manual/player/options/
 export const config = {
-  castPath: '/test2.cast',
-  duration: 15,
-  cols: 80,
-  rows: 24,
-  autoPlay: true,
-  fit: 'both',
-  width: 640,
-  height: 480,
-  fps: 24,
+  castPath: '/input.cast',  // file path relative to public
+  duration: 49.8,           // duration of video
+  cols: 80,                 // Window size rendered by asciinema
+  rows: 24,                 // Window size rendered by asciinema
+  autoPlay: true,           // output video true false
+  theme: 'asciinema',           // output video 
+  fit: 'both',              // 'width' 'height' 'both' 'none' 'width'
+  width: 1280,              // output video dimension
+  height: 720,              // output video dimension
+  fps: 24,                  // output video fps
 };
+
