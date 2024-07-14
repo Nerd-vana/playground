bae1f384a8d2a8aab9adfdd6f08030974c26a37e
BarryChum <barrychum@users.noreply.github.com>
2024-07-14 13:38:09 +0100

    updated to use config.ts

---
+++ asciinema2mp4/config.ts
@@ -0,0 +1,12 @@
+  // config.ts
+export const config = {
+  castPath: '/test2.cast',
+  duration: 15,
+  cols: 80,
+  rows: 24,
+  autoPlay: true,
+  fit: 'both',
+  width: 640,
+  height: 480,
+  fps: 24,
+};
